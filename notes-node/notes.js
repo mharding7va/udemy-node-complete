cbcad182ca1bed0c079b88bc9c5b552da34d4106
Michael Harding <mharding@genesisgroupusa.com>
2017-07-31 02:24:33 -0400

    Implemented Yargs to handle process args

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,25 @@
 console.log("Starting notes.js");
 
-// module.exports.age = 25;
-module.exports.addNote = () => {
-	console.log("addNote");
-	return "New Note";
+var addNote = (title, body) => {
+	console.log('Adding Note', title, body);
+};
+
+var getAll = () => {
+	console.log("Getting All Notes");
+};
+
+var getNote = (title) => {
+	console.log("Getting Note: ", title);
+};
+
+var removeNote = (title) => {
+	console.log("Removing Note: ", title);
+};
+
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
 }
